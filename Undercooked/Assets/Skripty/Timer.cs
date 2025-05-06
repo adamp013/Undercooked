b676936fe07fd976a5fed00136997ef4811e2a4c
adamp013 <adam.palko@outlook.com>
2025-05-06 18:03:38 +0200

    Timer
    Stopped, resumed

--- Undercooked/Assets/Skripty/Timer.cs
+++ Undercooked/Assets/Skripty/Timer.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
+    private bool isTimerRunning = true; // Flag to control timer state
     float elapsedTime = 0f;
 
     void Update()
     {
-        ElapsedTime();
+        if (isTimerRunning)
+        {
+            ElapsedTime();
+        }
     }
 
     void ElapsedTime()
@@ -18,5 +22,15 @@ public class Timer : MonoBehaviour
         int minutesT = Mathf.FloorToInt(elapsedTime / 60F);
         int secondsT = Mathf.FloorToInt(elapsedTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutesT, secondsT);
+    }
+
+    public void TimeStopped()
+    {
+        isTimerRunning = false;
+    }
+
+    public void TimeResumed()
+    {
+        isTimerRunning = true; 
     }
 }
