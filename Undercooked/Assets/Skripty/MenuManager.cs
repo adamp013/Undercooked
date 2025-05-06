ea61d474981a54f7cf875d65cc87343b22814def
adamp013 <adam.palko@outlook.com>
2025-05-06 12:16:47 +0200

    Main Menu, Levels

--- Undercooked/Assets/Skripty/MenuManager.cs
+++ Undercooked/Assets/Skripty/MenuManager.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    
+    public void LoadFirstScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("testScene");
+    }
+
+     public void LoadMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
    public void QuitGame()
    {
        Application.Quit();
